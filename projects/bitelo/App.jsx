e35ea1e2864b0b4cc0078fa122106ba14a9923d2
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-22 19:42:26 +0530

    feat: Initialize React project with basic app layout and logo assets.

--- projects/bitelo/App.jsx
+++ projects/bitelo/App.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1>This is title</h1>;
+const Header = () => (
+  <div className="header">
+    <div className="logo-box">
+      <img
+        className="logo"
+        src="https://as2.ftcdn.net/v2/jpg/02/20/52/39/1000_F_220523939_LLQ8aYBh0i8U3Ysyg6AV50NIHFxZwnB7.jpg"
+        alt="Imgae Unvailable!"
+        
+      ></img>
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+);
 
-const date = new Date().getDate();
-const month = new Date().getMonth();
-const year = new Date().getFullYear();
-
-const HeadingComponent = () => (
-  <div className="container">
-    {date}/{month}/{year}
-    <Title />
-    <h2 className="heading">This is H1 heading</h2>
+const AppLayout = () => (
+  <div className="app">
+    <Header />
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+
+root.render(<AppLayout />);
